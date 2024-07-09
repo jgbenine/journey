a39afb50bdb54077b31b3f3fba0aba37e0047dcc
jgbenine <joaobenine@gmail.com>
2024-07-09 17:39:12 -0300

    feat: rotas create e get links

--- backend/src/routes/get-activity.ts
+++ backend/src/routes/get-activity.ts
@@ -5,8 +5,7 @@ import z from "zod";
 import { dayjs } from "../lib/dayjs";
 
 export async function getActivities(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().get(
-    "/trips/:tripId/activities",
+  app.withTypeProvider<ZodTypeProvider>().get("/trips/:tripId/activities",
     {
       schema: {
         params: z.object({
@@ -19,25 +18,36 @@ export async function getActivities(app: FastifyInstance) {
 
       const trip = await prisma.trip.findUnique({
         where: { id: tripId },
-        include: { activities: true },
+        include: {
+          activities: {
+            orderBy: {
+              occours_at: "asc",
+            },
+          },
+        },
       });
 
       if (!trip) {
         throw new Error("Viagem não encontrada");
       }
 
-      const diferencaEntreInicioFinalViagem = dayjs(trip.ends_at).diff(trip.starts_at, 'days');
+      const diferencaEntreInicioFinalViagem = dayjs(trip.ends_at).diff(
+        trip.starts_at,
+        "days"
+      );
 
-      const activities = Array.from({length: diferencaEntreInicioFinalViagem + 1}).map((_, index)=>{
-        const date = dayjs(trip.starts_at).add(index, 'days');
+      const activities = Array.from({
+        length: diferencaEntreInicioFinalViagem + 1,
+      }).map((_, index) => {
+        const date = dayjs(trip.starts_at).add(index, "days");
 
         return {
           date: date.toDate(),
-          activities: trip.activities.filter(activity => {
-            return dayjs(activity.occours_at).isSame(date, 'day');
-          })
-        }
-      })
+          activities: trip.activities.filter((activity) => {
+            return dayjs(activity.occours_at).isSame(date, "day");
+          }),
+        };
+      });
 
       return { activities };
     }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,6 +7,8 @@ import { confirmParticipant } from "./routes/confirm-participant";
 import { createActivity } from "./routes/create-activity";
 import { getActivities } from "./routes/get-activity";
 import cors from "@fastify/cors"
+import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-links";
 
 const app = fastify();
 
@@ -26,6 +28,8 @@ app.register(confirmTrip)
 app.register(confirmParticipant)
 app.register(createActivity)
 app.register(getActivities)
+app.register(createLink)
+app.register(getLinks)
 
 
 app.listen({ port: 3333 }).then(() => {
