539f1744d60aa621b6b81d2a4c603834ab630059
jgbenine <joaobenine@gmail.com>
2024-07-10 20:09:00 -0300

    feat: traramento error

--- backend/src/error-handler.ts
+++ backend/src/error-handler.ts
@@ -1,8 +1,18 @@
 import type { FastifyInstance } from "fastify";
+import { ClientError } from "./errors/client-error";
+import { ZodError } from "zod";
 
 type FastifyErrorHandle = FastifyInstance['errorHandler']
 
 export const errorHandler: FastifyErrorHandle = (error, request, reply) =>{
-  console.log(error);
+
+  if(error instanceof ZodError){
+    return reply.status(400).send({message: error.flatten().fieldErrors})
+  }
+
+  if(error instanceof ClientError){
+    return reply.status(400).send({message: error.message})
+  }
+
   return reply.status(500).send({message: 'Internet Server Error'});
 }
