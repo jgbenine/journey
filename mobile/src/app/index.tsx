2308187f21c4ab7a4c0ec0e64c0c7b7ad3940aef
jgbenine <joaobenine@gmail.com>
2024-07-11 15:07:53 -0300

    add: steps formulario

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -3,37 +3,65 @@ import { colors } from "@/styles/colors"
 import { View, Text, StyleSheet, Image } from "react-native"
 import { MapPin, Calendar as IconCalendar, Settings2, UserRoundPlus, ArrowRight } from "lucide-react-native"
 import { Button } from "@/components/Button"
-import React from "react"
+import React, { useState } from "react"
 
+
+
+
+enum StepFormControl {
+  TRIP_DETAILS = 1,
+  EMAIL_DETAILS = 2,
+}
 
 export default function Index() {
+  const [stepForm, setStepForm] = useState(StepFormControl.TRIP_DETAILS)
+
+  function handleNextStepForm() {
+    if (stepForm === StepFormControl.TRIP_DETAILS) {
+      return setStepForm(StepFormControl.EMAIL_DETAILS)
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require("@/assets/logo.png")} resizeMode="contain" />
-      <Text style={styles.text}>Convide seus amigos e planeje sua {"\n"} melhor viagem!</Text>
 
+      <Image source={require(("@/assets/bg.png"))} style={styles.backgroundImage} />
+
+      <Text style={styles.text}>Convide seus amigos e planeje sua {"\n"} melhor viagem!</Text>
       <View style={styles.containerInput}>
         <Input>
           <MapPin color={colors.zinc[400]} size={18} />
-          <Input.Field placeholder="Para onde?" />
+          <Input.Field placeholder="Para onde?"
+            editable={stepForm === StepFormControl.TRIP_DETAILS}
+          />
         </Input>
         <Input>
           <IconCalendar size={18} color={colors.zinc[400]} />
-          <Input.Field placeholder="Quando?" />
+          <Input.Field placeholder="Quando?"
+            editable={stepForm === StepFormControl.TRIP_DETAILS}
+          />
         </Input>
 
-        <Button variant="secondary">
-          <Button.Title>Alterar local/data</Button.Title>
-          <Settings2 size={18} color={colors.zinc[200]} />
-        </Button>
-        <Text style={styles.divisor} />
-        <Input>
-          <UserRoundPlus size={18} color={colors.zinc[400]} />
-          <Input.Field placeholder="Quem vai viajar junto?" />
-        </Input>
+        {stepForm === StepFormControl.EMAIL_DETAILS && (
+          <>
+            <Button variant="secondary"
+              onPress={() => setStepForm(StepFormControl.TRIP_DETAILS)}>
+              <Button.Title>Alterar local/data</Button.Title>
+              <Settings2 size={18} color={colors.zinc[200]} />
+            </Button>
+            <Text style={styles.divisor} />
+            <Input>
+              <UserRoundPlus size={18} color={colors.zinc[400]} />
+              <Input.Field placeholder="Quem vai viajar junto?" />
+            </Input>
+          </>
+        )}
 
-        <Button>
-          <Button.Title>Continuar</Button.Title>
+        <Button onPress={handleNextStepForm}>
+          <Button.Title>
+            {stepForm === StepFormControl.TRIP_DETAILS ? "Continuar" : "Confirmar Viagem"}
+          </Button.Title>
           <ArrowRight size={18} color={colors.lime[950]} />
         </Button>
       </View>
@@ -46,11 +74,12 @@ export default function Index() {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center", paddingHorizontal: 20},
+  container: { flex: 1, justifyContent: "center", alignItems: "center", paddingHorizontal: 20 },
   text: { color: colors.zinc[400], textAlign: "center", fontSize: 20, marginTop: 10, lineHeight: 28 },
   logo: { height: 25 },
   containerInput: { width: "100%", backgroundColor: colors.zinc[800], paddingBottom: 15, paddingHorizontal: 10, borderRadius: 6, marginVertical: 40 },
   divisor: { padding: 1, marginVertical: 10, width: "100%", height: 1, backgroundColor: colors.zinc[500], opacity: 0.3 },
   termosPrivacidade: { color: colors.zinc[500], textAlign: "center", fontSize: 14, lineHeight: 20 },
-  termosHighlight: { color: colors.lime[300] }
+  termosHighlight: { color: colors.lime[300] },
+  backgroundImage: {position: "absolute"}
 })
