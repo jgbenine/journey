54347b781d8fc67fab7b3328eac98ec9b3e94f03
jgbenine <joaobenine@gmail.com>
2024-07-10 21:04:45 -0300

    layout+init index

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,10 +1,18 @@
-import { View, Text } from "react-native"
+import { colors } from "@/styles/colors"
+import { View, Text, StyleSheet, Image } from "react-native"
 
 
 export default function Index(){
   return (
-    <View>
-      <Text>Home Page</Text>
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require("@/assets/logo.png")} resizeMode="contain" />
+      <Text style={styles.text}>Convide seus amigos e planeje sua {"\n"} melhor viagem!</Text>
     </View>
   ) 
 }
+
+const styles = StyleSheet.create({
+  container: {flex: 1,  justifyContent: "center", alignItems: "center"},
+  text: {color: colors.zinc[400], textAlign: "center", fontSize: 20, marginTop: 10, lineHeight: 28},
+  logo: {height: 25}
+})
