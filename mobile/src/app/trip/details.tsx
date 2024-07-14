fb39bbb66d4819a3ee503e7159f04375782a6219
jgbenine <joaobenine@gmail.com>
2024-07-14 19:55:56 -0300

    feat: initial details section

--- mobile/src/app/trip/details.tsx
+++ mobile/src/app/trip/details.tsx
@@ -1,10 +1,68 @@
-import { View } from "lucide-react-native"
-import { StyleSheet, Text } from "react-native"
+import { useState } from "react"
+import { Button } from "@/components/Button"
+import { Modal } from "@/components/modal"
+import { colors } from "@/styles/colors"
+import { Plus } from "lucide-react-native"
+import { StyleSheet, Text, View } from "react-native"
+import { Input } from "@/components/Input"
 
-export default function Details({tripId} : {tripId: string}) {
+export default function Details({ tripId }: { tripId: string }) {
+  const [showModal, setShowModal] = useState(false);
+  const [linkName, setLinkName] = useState("");
+  const [linkURL, setLinkURL] = useState("");
+  const [isCreatingLinkTrip, setIsCreatingLinkTrip] = useState(false)
+
+
+  async function handleCreateLinkTrip(){
+    try {
+      
+    } catch (error) {
+      console.log(error) 
+    }finally{
+      setIsCreatingLinkTrip(false);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{tripId}</Text>
+      <Text style={styles.titleIntro}>Links Importantes</Text>
+
+      <View style={styles.content}>
+        <Button variant="secondary" onPress={() => setShowModal(true)}>
+          <Plus color={colors.zinc[200]} />
+          <Button.Title>
+            Cadastrar novo link
+          </Button.Title>
+        </Button>
+      </View>
+
+      <Modal
+        title="Cadastrar Link"
+        subtitle="Todos convidados podem visualizar os links."
+        visible={showModal}
+        onClose={() => setShowModal(false)}>
+
+        <View style={styles.contentModal}>
+          <Input variants="secondary">
+            <Input.Field
+              placeholder="Titulo do link"
+              onChangeText={setLinkName}
+            />
+          </Input>
+          <Input variants="secondary">
+            <Input.Field
+              placeholder="Url do link"
+              onChangeText={setLinkURL}
+            />
+          </Input>
+        </View>
+
+        <Button isLoading={isCreatingLinkTrip} onPress={handleCreateLinkTrip}>
+          <Button.Title>
+            Salvar link
+          </Button.Title>
+        </Button>
+      </Modal>
     </View>
   )
 }
@@ -12,10 +70,30 @@ export default function Details({tripId} : {tripId: string}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+
   },
 
-  text: {
-    color: "white"
+  content: {
+    width: '100%',
+    flexDirection: "row",
+    marginTop: 15,
+    marginBottom: 20,
+  },
+
+  titleIntro: {
+    color: "white",
+    fontSize: 26,
+    fontWeight: "semibold",
+    flex: 1,
+    marginTop: 10,
+  },
+
+  contentModal: {
+    gap: 8,
+    marginBottom: 12,
   }
+
+
+
 
 })
